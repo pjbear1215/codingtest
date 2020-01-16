e33572ca3c2ab9978aa34fd23c5203dacc67a8d7
jwpark <pjbear1215@gmail.com>
2020-01-17 01:33:00 +0900

    vector test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace::std;
 
 int main(void) {
-    cout << "Hello World.\n";
+    vector<string> v;
+    v.push_back("I");
+    v.push_back("am");
+    v.push_back("a");
+    v.push_back("student");
+    v.push_back(".");
+
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+    
+    v.pop_back();
+    vector<string>::iterator iter;
+    for (iter = v.begin(); iter != v.end(); iter++) {
+        if (*iter == "a") {
+            cout << *iter << " is erased" << endl;
+            v.erase(iter);
+            break;
+        }        
+    }
+
+    for (iter = v.begin(); iter != v.end(); iter++) {
+        cout << *iter << endl;
+    }
+
+    cout << endl << endl;
+
+    cout << "v.size() = " << v.size() << endl << "v.capacity() = " << v.capacity() << endl;
+
     return 0;
 }
